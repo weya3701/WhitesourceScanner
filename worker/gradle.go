ef6ec05e58c5fde5241d13751ff07f2f4574fccd
Jonas Yang <wen-hsin.yang@colorado.edu>
2025-12-24 15:01:35 +0800

    feature: add worker

---
+++ worker/gradle.go
@@ -0,0 +1,76 @@
+package worker
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+type Gradle struct{}
+
+func (gradle Gradle) Download(destination string, packageName string, indexUrl string) string {
+	var cmd string
+	cmd = fmt.Sprintf("pip download %s --dest %s/%s/ %s", indexUrl, destination, packageName, packageName)
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		panic(err)
+	}
+	return string(out)
+}
+
+func (gradle Gradle) Sync(targetUrl string, packageFile string) string {
+
+	apiUrl := targetUrl
+	file, err := os.Open(packageFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	requestBody := &bytes.Buffer{}
+	writer := multipart.NewWriter(requestBody)
+	part, err := writer.CreateFormFile("file", filepath.Base(packageFile))
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(part, file)
+	if err != nil {
+		panic(err)
+	}
+	writer.Close()
+
+	request, err := http.NewRequest("POST", apiUrl, requestBody)
+	if err != nil {
+		panic(err)
+	}
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+	request.SetBasicAuth("admin", "admin")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(body)
+
+}
+
+func (gradle Gradle) Remove(packageName string) error {
+	fullPath := fmt.Sprintf("./tmp/%s", packageName)
+	err := os.RemoveAll(fullPath)
+	return err
+
+}
---
+++ worker/npm.go
@@ -0,0 +1,76 @@
+package worker
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+type Npm struct{}
+
+func (npm Npm) Download(destination string, packageName string, indexUrl string) string {
+	var cmd string
+	cmd = fmt.Sprintf("pip download %s --dest %s/%s/ %s", indexUrl, destination, packageName, packageName)
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		panic(err)
+	}
+	return string(out)
+}
+
+func (npm Npm) Sync(targetUrl string, packageFile string) string {
+
+	apiUrl := targetUrl
+	file, err := os.Open(packageFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	requestBody := &bytes.Buffer{}
+	writer := multipart.NewWriter(requestBody)
+	part, err := writer.CreateFormFile("file", filepath.Base(packageFile))
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(part, file)
+	if err != nil {
+		panic(err)
+	}
+	writer.Close()
+
+	request, err := http.NewRequest("POST", apiUrl, requestBody)
+	if err != nil {
+		panic(err)
+	}
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+	request.SetBasicAuth("admin", "admin")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(body)
+
+}
+
+func (npm Npm) Remove(packageName string) error {
+	fullPath := fmt.Sprintf("./tmp/%s", packageName)
+	err := os.RemoveAll(fullPath)
+	return err
+
+}
