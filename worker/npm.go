e26250dcead266219cb1b45640c996efa4102374
Jonas Yang <wen-hsin.yang@colorado.edu>
2026-01-01 14:51:57 +0800

    feature: 新增npm取得套件功能

--- worker/npm.go
+++ worker/npm.go
@@ -24,6 +24,38 @@ func (npm Npm) Download(destination string, packageName string, indexUrl string)
 	return string(out)
 }
 
+func (npm Npm) SyncPackages(destination string, requirementsFile string) error {
+
+	packageTmp := os.Getenv("package_tmp")
+	if packageTmp == "" {
+		return fmt.Errorf("package_tmp is empty")
+	}
+
+	downloadDestination := fmt.Sprintf("%s/%s", packageTmp, destination)
+	if err := os.MkdirAll(downloadDestination, 0755); err != nil {
+		return fmt.Errorf("Create Dir failed: %w", err)
+	}
+	// copy package.json to downloadDestination filder
+	copyCmd := []string{requirementsFile, downloadDestination}
+	fmt.Println(copyCmd)
+	cpcmd := exec.Command("cp", copyCmd...)
+	cpout, err := cpcmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Copy package.json failed: %w, output: %s", err, string(cpout))
+	}
+
+	// npm install --prefix ./my-target-folder
+	cmdArgs := []string{"install", "-prefix", downloadDestination}
+	fmt.Println(cmdArgs)
+	cmd := exec.Command("npm", cmdArgs...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("pip download failed: %w, output: %s", err, string(out))
+	}
+
+	return nil
+}
+
 func (npm Npm) Sync(targetUrl string, packageFile string) string {
 
 	apiUrl := targetUrl
