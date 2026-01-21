a495370c0cd057bfba8328b03843205e81c46cc1
Jonas Yang <wen-hsin.yang@colorado.edu>
2026-01-21 11:54:19 +0800

    chore: 新增註解，程式碼片段說明

--- worker/npm.go
+++ worker/npm.go
@@ -15,6 +15,23 @@ func (npm Npm) Download(destination string, packageName string, indexUrl string)
 	return string(cmd)
 }
 
+// SyncPackages 函數同步 npm 套件。
+//
+// 參數：
+//   - destination:  要同步套件的目的地目錄名稱 (例如 "package1")。此名稱將用作子目錄，在 package_tmp 和 report_tmp 目錄中創建。
+//   - requirementsFile:  package.json 檔案的完整路徑，該檔案包含要安裝的套件。  此檔案將被複製到下載目錄中。
+//
+// 返回值：
+//   - error:  如果發生任何錯誤，將返回一個錯誤。否則，返回 nil 表示成功。
+//
+// 流程：
+//  1. 獲取 "package_tmp" 和 "report_tmp" 環境變數的值。
+//  2. 檢查 "package_tmp" 是否為空。如果是，則返回錯誤。
+//  3. 根據 "package_tmp"、"report_tmp" 和提供的目的地創建下載和報告目錄的完整路徑。
+//  4. 使用 os.MkdirAll 創建下載和報告目錄。如果目錄已存在，則不會返回錯誤。
+//  5. 將 requirementsFile (package.json) 檔案複製到下載目錄。
+//  6. 使用 `npm install --prefix <downloadDestination>` 命令，在下載目錄中安裝套件。
+//  7. 如果在任何步驟中發生錯誤，則返回錯誤。
 func (npm Npm) SyncPackages(destination string, requirementsFile string) error {
 
 	var err error = nil
