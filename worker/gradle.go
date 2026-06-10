18a018b9506d9d06dd5e73e38b3c9b0815d53c2a
Jonas Yang <wen-hsin.yang@colorado.edu>
2026-06-10 18:10:55 +0800

    chore: update parameter

--- worker/gradle.go
+++ worker/gradle.go
@@ -3,10 +3,8 @@ package worker
 import (
 	"context"
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"time"
 )
 
@@ -20,45 +18,45 @@ type ReplaceRule struct {
 }
 
 // FIXME. 移至檔案操作模組
-func copyFile(sourcePath, destinationDir string) error {
-	// 1. 檢查源檔案是否存在
-	sourceFile, err := os.Open(sourcePath)
-	if err != nil {
-		return fmt.Errorf("無法開啟源檔案 '%s': %w", sourcePath, err)
-	}
-	defer sourceFile.Close()
-
-	// 2. 獲取源檔案的檔案資訊 (用於獲取檔案名稱)
-	fileInfo, err := sourceFile.Stat()
-	if err != nil {
-		return fmt.Errorf("無法獲取源檔案資訊 '%s': %w", sourcePath, err)
-	}
-	fileName := fileInfo.Name()
-
-	// 3. 創建目標檔案的路徑
-	destinationPath := filepath.Join(destinationDir, fileName)
-
-	// 4. 創建目標檔案
-	destinationFile, err := os.Create(destinationPath)
-	if err != nil {
-		return fmt.Errorf("無法創建目標檔案 '%s': %w", destinationPath, err)
-	}
-	defer destinationFile.Close()
-
-	// 5. 複製檔案內容
-	_, err = io.Copy(destinationFile, sourceFile)
-	if err != nil {
-		return fmt.Errorf("複製檔案內容錯誤: %w", err)
-	}
-
-	// 6. 複製檔案權限 (如果需要的話)
-	err = os.Chmod(destinationPath, fileInfo.Mode())
-	if err != nil {
-		fmt.Printf("警告: 無法設置目標檔案權限 '%s': %v\n", destinationPath, err)  // 不返回錯誤，繼續執行
-	}
-
-	return nil
-}
+// func copyFile(sourcePath, destinationDir string) error {
+// 	// 1. 檢查源檔案是否存在
+// 	sourceFile, err := os.Open(sourcePath)
+// 	if err != nil {
+// 		return fmt.Errorf("無法開啟源檔案 '%s': %w", sourcePath, err)
+// 	}
+// 	defer sourceFile.Close()
+//
+// 	// 2. 獲取源檔案的檔案資訊 (用於獲取檔案名稱)
+// 	fileInfo, err := sourceFile.Stat()
+// 	if err != nil {
+// 		return fmt.Errorf("無法獲取源檔案資訊 '%s': %w", sourcePath, err)
+// 	}
+// 	fileName := fileInfo.Name()
+//
+// 	// 3. 創建目標檔案的路徑
+// 	destinationPath := filepath.Join(destinationDir, fileName)
+//
+// 	// 4. 創建目標檔案
+// 	destinationFile, err := os.Create(destinationPath)
+// 	if err != nil {
+// 		return fmt.Errorf("無法創建目標檔案 '%s': %w", destinationPath, err)
+// 	}
+// 	defer destinationFile.Close()
+//
+// 	// 5. 複製檔案內容
+// 	_, err = io.Copy(destinationFile, sourceFile)
+// 	if err != nil {
+// 		return fmt.Errorf("複製檔案內容錯誤: %w", err)
+// 	}
+//
+// 	// 6. 複製檔案權限 (如果需要的話)
+// 	err = os.Chmod(destinationPath, fileInfo.Mode())
+// 	if err != nil {
+// 		fmt.Printf("警告: 無法設置目標檔案權限 '%s': %v\n", destinationPath, err)  // 不返回錯誤，繼續執行
+// 	}
+//
+// 	return nil
+// }
 
 func getBuildTemplate() string {
 	return `task downloadDependencies(type: Copy) {
@@ -196,7 +194,7 @@ func (gradle Gradle) SyncPackages(destination string, requirementsFile string) e
 	dependenciesTreeFile := fmt.Sprintf("%s/dependenciesTree.txt", reportDestination)
 	fmt.Println("dependencies tree file: ", dependenciesTreeFile)
 	cmds := []string{"dependencies"}
-	err = GetDependenciesTree(dependenciesTreeFile, os.Getenv("gradle"), cmds)
+	err = GetDependenciesTree(dependenciesTreeFile, gradle.Command, cmds)
 
 	if err != nil {
 		return fmt.Errorf("Get dependencies tree failed: %w", err)
