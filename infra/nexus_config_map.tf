bd05569de3bebde35df3e0d557a8c7fbb2bb3dae
Jonas Yang <wen-hsin.yang@colorado.edu>
2026-07-17 14:49:38 +0800

    chore: remove modules

--- infra/nexus_config_map.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_config_map" "nexuspropertiesdemo" {
-  metadata {
-    name = "nexus-properties"
-  }
-
-  data = {
-    "nexus.properties" = "${file("./conf/nexus.properties")}"
-  }
-}
